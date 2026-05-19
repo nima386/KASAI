67e77afa4c9d0abdfc8f3138a7dc991b438e04ce
nima386 <re.nima786@gmail.com>
2026-05-19 21:36:20 +0200

    Add files via upload

--- js/core/version.js
+++ js/core/version.js
@@ -1,5 +1,5 @@
 (function(){
-  const current = 'kasai-v43-timepush-i18n6-ux1';
+  const current = 'kasai-v43-timepush-i18n6-ux2';
   const buildDate = '2026-05-19';
 
   window.KasaiVersion = Object.freeze({
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'kasai-v43-timepush-i18n6-ux1';
+const CACHE = 'kasai-v43-timepush-i18n6-ux2';
 const ASSETS = [
   './',
   './index.html',
